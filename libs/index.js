4f063aecaf227f58b8a4a85d2cbb1cff0bc8a3da
Programmer707707 <toxirovsherzod707@gmail.com>
2025-01-20 16:14:58 +0500

    Account & Transaction routes and controllers completed

--- libs/index.js
+++ libs/index.js
@@ -28,3 +28,22 @@ export const generateToken = (id) =>{
         }
     );
 }
+
+
+export function getMonthName(index) {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return months[index];
+}
